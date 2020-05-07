5960b30f11d483074e47940b026a137f7826ee62
Ivan Goncalves <ivangg@gmail.com>
2020-05-07 18:07:47 -0300

    Updates

---
+++ precache-manifest.8c8f744d0084a71876d8e5cf954982be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d696843041791e2db0b9e691400eed41",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e19aa8fcf17bb4e6da42",
+    "url": "/monsters-rolodex/static/css/main.e1f36e54.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19aa8fcf17bb4e6da42",
+    "url": "/monsters-rolodex/static/js/main.d00bfb46.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
